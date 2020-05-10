c412d53ce86a0aa00a3472c61ba942e458cf1b4f
Palash Sharma <54630543+palashsharma891@users.noreply.github.com>
2020-05-10 17:38:05 +0530

    Create ps0.py

---
+++ Problem Set 0/ps0.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  5 15:20:18 2020
+
+@author: Palash
+"""
+
+import math
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+print("x**y = ", x**y)
+print("log(x) = ", math.log(x))
